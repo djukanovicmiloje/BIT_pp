02b77a727048c98577dc260c8eb6cd3285d067ab
BIT Student <classroom@bgit.rs>
2019-10-15 17:59:49 +0200

    Switch i Nizovi

---
+++ Switch/switch.js
@@ -0,0 +1,198 @@
+/* var dan = 3;
+switch (dan) {
+    case 1:
+        console.log("Ponedeljak");
+        break;
+    case 2:
+        console.log("Utorak");
+        break;
+    case 3:
+        console.log("Sreda");
+        break;
+    case 4:
+        console.log("Cetvrtak");
+        break;
+    case 5:
+        console.log("Petak");
+        break;
+    case 6:
+        console.log("Subota");
+        break;
+    case 7:
+        console.log("Nedelja");
+        break;
+} */
+/* var dan = 52;
+switch (dan) {
+    case 1:
+        console.log("Ponedeljak");
+        break;
+    case 2:
+        console.log("Radni je dan");
+        break;
+    case 3:
+        console.log("Sreda");
+        break;
+    case 4:
+        console.log("Cetvrtak");
+        break;
+    case 5:
+        console.log("Petak");
+        break;
+    case 6:
+        console.log("Vikend je");
+        break;
+    case 7:
+        console.log("Nedelja");
+        break;
+    default:
+        console.log("Broj mora da bude izmedju 1 i 7");
+} */
+/* var mesec = 13;
+switch (mesec) {
+    case 2:
+        console.log("Februar");
+        break;
+    case 6:
+        console.log("JUNI JE");
+        break;
+    case 13:
+        console.log("Nece da moze");
+        break;
+}
+ */
+/* var mesec = 5;
+var godDoba = "";
+if (mesec === 12 || mesec === 1 || mesec === 2) godDoba = "zima";
+if (mesec === 3 || mesec === 4 || mesec === 5) godDoba = "prolece";
+if (mesec === 6 || mesec === 7 || mesec === 8) godDoba = "leto";
+if (mesec === 9 || mesec === 10 || mesec === 11) godDoba = "jesen";
+
+switch (mesec) {
+    case 12:
+
+    case 1:
+
+    case 2:
+        console.log("zima je");
+        break;
+    case 3:
+
+    case 4:
+
+    case 5:
+        console.log("prolece je");
+        break;
+    case 6:
+
+    case 7:
+
+    case 8:
+        console.log("let je");
+        break;
+    case 9:
+
+    case 10:
+
+    case 11:
+        console.log("jesen je");
+        break;
+
+} */
+/* var ocena = "vazi";
+
+switch (ocena) {
+    case "A":
+        console.log("Good Job");
+        break;
+    case "B":
+        console.log("Pretty Good");
+        break;
+    case "C":
+        console.log("Passed");
+        break;
+    case "D":
+        console.log("Not So Good");
+        break;
+    case "F":
+        console.log("Failed");
+        break;
+    default:
+        console.log("Unkown Grade");
+        break;
+}
+ */
+/* var city = "Paris";
+
+switch (city) {
+    case "Beograd":
+    case "Nis":
+    case "Novi Sad":
+    case "Kragujevac":
+    case "Subotica":
+        console.log("Srbija");
+        break;
+    case "London":
+    case "Liverpool":
+    case "Manchester":
+    case "Engleska1":
+    case "Engleska2":
+        console.log("Engleska");
+        break;
+    case "Paris":
+    case "Dijon":
+    case "Marsaille":
+    case "Nica":
+    case "Pariz2":
+        console.log("Francuska");
+        break;
+
+}
+ */
+/* var broj1 = 5;
+var broj2 = 0;
+var operacija = "+";
+
+switch (operacija) {
+    case "+":
+        console.log(broj1 + broj2);
+        break;
+    case "-":
+        console.log(broj1 - broj2);
+        break;
+    case "*":
+        console.log(broj1 * broj2);
+        break;
+    case "/":
+        if (broj2 !== 0) console.log(broj1 / broj2);
+        else console.log("cant divide by 0");
+        break;
+
+} */
+
+/* var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+console.log(months[0], months[5], months[9]);
+ */
+/* var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+console.log(week[0]); */
+/* var array = [2, -4, 5, -2, -11];
+var i;
+
+for (i = 0; i < 5; i++) {
+    if (array[i] < 0) console.log(array[i]);
+}
+
+if (array[0] < 0) console.log(array[0]);
+if (array[1] < 0) console.log(array[1]);
+if (array[2] < 0) console.log(array[2]);
+if (array[3] < 0) console.log(array[3]);
+if (array[4] < 0) console.log(array[4]); */
+
+/* var array = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+for (var i = 0; i < 9; i++) {
+    if (i % 3 === 0) console.log(array[i]);
+} */
+/* var array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(array[2]);
+var array2 = array[2];
+console.log(array2[1]); */
---
+++ form.js
@@ -0,0 +1,7 @@
+var a, b;
+a = 5;
+b = 6;
+if (a > b) {
+    console.log("vazi");
+    b = a;
+}
